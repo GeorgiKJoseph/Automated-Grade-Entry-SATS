d837cc19cd209a90262966eb3285192dc2cc51dc
georgikjoseph <georgikjoseph@gmail.com>
2020-06-12 21:26:57 +0530

    added: Login, nav to data entry page

--- app.bp.py
+++ app.bp.py
@@ -1,4 +1,3 @@
-# ERROR:
 import os
 
 from selenium import webdriver
@@ -6,27 +5,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from time import sleep
 
-# Authethication credentials
-USERNAME = 'kvafsyel'
-PASSWORD = '18$%KVGs'
-
-# Class Details
-ACADEMIC_YEAR = '2019-2020'
-MEDIUM = '19'   # English :- 19
-STANDARD = '9'
-EXAM = '1S2'
-GROUP = '1044'  # EM_Eng_Hin_San :- 1044
-
 # Find dynamic location
 location = os.getcwd()
 test_link = 'file://' + location + '/webpage/STS%20|%20Student%20Result.htm#'
 
-# Online link
-link = 'https://sts.karnataka.gov.in/SATS/#'
-
 # Setting webdriver (uses Firefox geckodriver)
 driver = webdriver.Firefox()
-driver.get(link)
+driver.get(test_link)
 
 # File Operator
 class FOperator:
@@ -83,8 +68,6 @@ class FOperator:
 # Bot
 class Bot:
     def __init__(self):
-        self.navToEntryPage()
-        self.selectSection()
         names = self.getNames()
         for i in range(len(names)):
             name_file_index = FOperator.checkName(names[i])
@@ -92,97 +75,11 @@ class Bot:
                 marks = FOperator.getMarks(name_file_index)
                 self.setGrades(i,marks)
 
-    # Navigates to data entry page
-    def navToEntryPage(self):
-        # Home -> Login page
-        xp = '/html/body/div/div[2]/div/div/nav/ul/li[8]/a'
-        driver.find_element_by_xpath(xp).click()
-
-        # Authethication at Login
-        # username textfield xpath
-        #   //*[@id="userName"]
-        uname_xp = '//*[@id="userName"]'
-        # password textfield xpath
-        #   //*[@id="xxZTT9p2wQ"]
-        pass_xp = '//*[@id="xxZTT9p2wQ"]'
-        # captcha textfield xpath
-        #   //*[@id="txtcode"]
-        captch_xp = '//*[@id="txtcode"]'
-
-        # Get captcha
-        # xpath :-  //*[@id="randomfield"]
-        xp = '//*[@id="randomfield"]'
-        captcha = driver.find_element_by_xpath(xp)
-
-        # Username, Password & Captcha entry
-        driver.find_element_by_xpath(uname_xp).send_keys(USERNAME)
-        driver.find_element_by_xpath(pass_xp).send_keys(PASSWORD)
-        driver.find_element_by_xpath(captch_xp).send_keys(captcha.text)
-
-        # Login button
-        xp = '//*[@id="xmlLogin"]'
-        driver.find_element_by_xpath(xp).click()
-
-        # Select 1-10 -> CCE results -> result Form
-        xp = '/html/body/div[3]/div[2]/div/div[3]/table/tbody/tr/td/table/tbody/tr/td[1]/div/table/tbody/tr/td/table/tbody/tr/td/div[2]/ul/li[4]/a'
-        element = driver.find_element_by_xpath(xp)
-        element.click()
-        sleep(1)
-        xp = '/html/body/div[3]/div[2]/div/div[3]/table/tbody/tr/td/table/tbody/tr/td[1]/div/table/tbody/tr/td/table/tbody/tr/td/div[2]/ul/li[4]/ul/li[5]/a'
-        element = driver.find_element_by_xpath(xp)
-        element.click()
-        sleep(1)
-        xp = '/html/body/div[3]/div[2]/div/div[3]/table/tbody/tr/td/table/tbody/tr/td[1]/div/table/tbody/tr/td/table/tbody/tr/td/div[2]/ul/li[4]/ul/li[5]/ul/li[2]/a'
-        element = driver.find_element_by_xpath(xp)
-        element.click()
-
-    def selectSection(self):
-        # Select Academic year   xpath :- //*[@id="year"]
-        xp = '//*[@id="year"]'
-        element = driver.find_element_by_xpath(xp)
-        select = Select(element)
-        select.select_by_value(ACADEMIC_YEAR)
-        sleep(2)
-
-        # Select Medium  xpath :- //*[@id="stu_schoolmedium"]
-        xp = '//*[@id="stu_schoolmedium"]'
-        element = driver.find_element_by_xpath(xp)
-        select = Select(element)
-        select.select_by_value(MEDIUM)
-        sleep(2)
-
-        # Select Standard xpath :- //*[@id="std"]
-        xp = '//*[@id="std"]'
-        element = driver.find_element_by_xpath(xp)
-        select = Select(element)
-        select.select_by_value(STANDARD)
-        sleep(2)
-
-        # Select Exam xpath :- //*[@id="semester_id"]
-        xp = '//*[@id="semester_id"]'
-        element = driver.find_element_by_xpath(xp)
-        select = Select(element)
-        select.select_by_value(EXAM)
-        sleep(2)
-
-        # Select Exam xpath :- //*[@id="group"]
-        xp = '//*[@id="group"]'
-        element = driver.find_element_by_xpath(xp)
-        select = Select(element)
-        select.select_by_value(GROUP)
-        sleep(2)
-
-        # Search
-        xp = '/html/body/div[3]/div[2]/div/div[3]/table/tbody/tr/td/table/tbody/tr/td[2]/div/form[1]/fieldset/table/tbody/tr[7]/td/input[1]'
-        driver.find_element_by_xpath(xp).click()
-        sleep(20)
 
     # Returns an array with all student names
     def getNames(self):
-        print('Listing names...')
         count = 1
-        #pre_xp = '/html/body/div[3]/div[2]/div/div[3]/table/tbody/tr/td/table/tbody/tr/td[2]/div/form[2]/div[3]/div[3]/table/tbody/tr['
-        pre_xp = '/html/body/div[3]/div[2]/div/div[3]/table/tbody/tr/td/table/tbody/tr/td[2]/div/form[2]/div[3]/table/tbody/tr['
+        pre_xp = '/html/body/div[3]/div[2]/div/div[3]/table/tbody/tr/td/table/tbody/tr/td[2]/div/form[2]/div[3]/div[3]/table/tbody/tr['
         post_xp = ']/td[3]'
         xp = pre_xp + str(count) + post_xp
         names = []
@@ -198,7 +95,6 @@ class Bot:
                 ele = driver.find_element_by_xpath(xp)
             except:
                 ele = None
-        print(names)
         return names
 
     # Select grade in dropdown
@@ -217,12 +113,11 @@ class Bot:
     '''
     # marks = [ENG,HINDI,MATHS,SOCIAL,SCIENCE]
     def setGrades(self,index,marks):
-        #pre_xp = '/html/body/div[3]/div[2]/div/div[3]/table/tbody/tr/td/table/tbody/tr/td[2]/div/form[2]/div[3]/div[3]/table/tbody/tr['
-        pre_xp = '/html/body/div[3]/div[2]/div/div[3]/table/tbody/tr/td/table/tbody/tr/td[2]/div/form[2]/div[3]/table/tbody/tr['
+        pre_xp = '/html/body/div[3]/div[2]/div/div[3]/table/tbody/tr/td/table/tbody/tr/td[2]/div/form[2]/div[3]/div[3]/table/tbody/tr['
         mid_xp = ']/td['
         post_xp = ']'
         index += 1
-
+        
         # English  xpath :-  //*[@id="firstl_id"] , col = 5
         xp = pre_xp + str(index) + mid_xp + '5' + post_xp
         print(str(index))
